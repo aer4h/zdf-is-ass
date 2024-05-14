3ed119897c4effd9fe0dbc6e2eb8eaecb303888f
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2024-05-14 15:07:45 +0800

    Update bindtp.lua

--- gui/bindtp.lua
+++ gui/bindtp.lua
@@ -1,14 +1,22 @@
+local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+	Title = "CTRL To TP";
+	Text = "Keybind: CTRL";
+	Duration = 5;
+})
+
 local Plr = game:GetService("Players").LocalPlayer
 local Mouse = Plr:GetMouse()
- 
+
 Mouse.Button1Down:connect(
-    function()
-        if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then
-            return
-        end
-        if not Mouse.Target then
-            return
-        end
-        Plr.Character:MoveTo(Mouse.Hit.p)
-    end
+	function()
+		if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then
+			return
+		end
+		if not Mouse.Target then
+			return
+		end
+		Plr.Character:MoveTo(Mouse.Hit.p)
+	end
 )
